59f5534c550f869e025ff41c87ab6649d0e379a5
jamestq <57053746+jamestq@users.noreply.github.com>
2023-03-31 12:02:58 +1100

    remove extra folders

--- dictclient/src/app/test/Test.java
+++
@@ -1,9 +0,0 @@
-package app.test;
-
-import app.components.ClientWindow;
-
-public class Test{
-    public static void main(String[] args){
-        ClientWindow newWindow = new ClientWindow();
-    }
-}
